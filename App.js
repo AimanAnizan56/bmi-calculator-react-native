bf65eb29cad0391bc3ef86c4c6788eca82c1594f
Aiman Anizan <anizanaimann1@gmail.com>
2023-04-04 15:07:24 +0800

    Add gender state

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { Platform, NativeModules, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import GenderSelect from './components/GenderSelect';
 import Navbar from './components/Navbar';
 
 const { StatusBarManager } = NativeModules;
 
 export default function App() {
+  const [gender, setGender] = useState('male');
+
   return (
     <SafeAreaView style={styles.status_bar}>
       <StatusBar style="auto" />
       <Navbar />
+      <GenderSelect gender={gender} setGender={setGender} />
     </SafeAreaView>
   );
 }
