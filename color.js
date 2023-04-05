3ea74f593ea9b1afdf4959e4c9f01f1ab6a6546b
Aiman Anizan <anizanaimann1@gmail.com>
2023-04-05 13:08:10 +0800

    Add color

--- color.js
+++ color.js
@@ -1,4 +1,7 @@
 export const COLOR_GRAY = '#aeaeae';
 export const COLOR_BLACK = '#484848';
+export const COLOR_BLACK__DISABLED = 'rgba(72,72,72,0.5)';
 export const COLOR_ORANGE = '#ff7f33';
 export const COLOR_PINK = '#ff1d8e';
+export const COLOR_WHITE = '#ffffff';
+export const COLOR_WHITE__DISABLED = 'rgba(255,255,255,0.5)';
