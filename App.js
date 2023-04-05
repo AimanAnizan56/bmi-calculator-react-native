2e366036e8cf5913f0790e0601ca87a128c0dcee
Aiman Anizan <anizanaimann1@gmail.com>
2023-04-05 11:58:07 +0800

    Add detail for height, weight and age

--- App.js
+++ App.js
@@ -2,18 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { Platform, NativeModules, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
 import GenderSelect from './components/GenderSelect';
-import Navbar from './components/Navbar';
+import { Details, Navbar } from './components';
 
 const { StatusBarManager } = NativeModules;
 
 export default function App() {
   const [gender, setGender] = useState('male');
+  const [height, setHeight] = useState();
+  const [weight, setWeight] = useState();
+  const [age, setAge] = useState();
 
   return (
     <SafeAreaView style={styles.status_bar}>
       <StatusBar style="auto" />
       <Navbar />
       <GenderSelect gender={gender} setGender={setGender} />
+      <Details textHeader="Height (in cm)" placeholder="0cm" currentState={height} setCurrentState={setHeight} />
+      <Details textHeader="Weight (in kg)" placeholder="0kg" currentState={weight} setCurrentState={setWeight} />
+      <Details textHeader="Age" placeholder="0yr" currentState={age} setCurrentState={setAge} />
     </SafeAreaView>
   );
 }
